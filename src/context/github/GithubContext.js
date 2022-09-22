b32eda33696e77162b6213744ebb8fd9b344383f
Avni Piro <90833537+PiroAvni@users.noreply.github.com>
2022-09-22 18:07:37 +0100

    Reducers, context update

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+        // users: state.users,
+        // loading: state.loading,
+        // user: state.user,
+        // repos:state.repos,
+        //fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
